2c5fcbf01ae6f031655f6f182b3ee29c09731302
Elle <hw45512@horwang.ac.th>
2023-11-29 11:39:05 +0700

    add button number

--- main.py
+++ main.py
@@ -10,8 +10,65 @@ class MyCalculator:
        self.label = tk.Label(self.root, text="Hello DIP01", font=('Arial', 18))
        self.label.pack()
 
-       self.button = tk.Button(self.root, text="CLick here", height=4)
+       self.button = tk.Button(self.root, text="C", height=2, width=4, font=('Arial',18))
        self.button.place(x=20,y=50)
+
+       self.button = tk.Button(self.root, text="CE", height=2,width=4, font=('Arial',18))
+       self.button.place(x=120,y=50)
+
+       self.button = tk.Button(self.root, text="%", height=2,width=4, font=('Arial',18))
+       self.button.place(x=220,y=50)
+
+       self.button = tk.Button(self.root, text="+", height=2,width=4, font=('Arial',18))
+       self.button.place(x=320,y=50)
+
+       self.button = tk.Button(self.root, text="7", height=2,width=4, font=('Arial',18))
+       self.button.place(x=20,y=150)
+
+       self.button = tk.Button(self.root, text="8", height=2,width=4, font=('Arial',18))
+       self.button.place(x=120,y=150)
+
+       self.button = tk.Button(self.root, text="9", height=2,width=4, font=('Arial',18))
+       self.button.place(x=220,y=150)
+
+       self.button = tk.Button(self.root, text="-", height=2,width=4, font=('Arial',18))
+       self.button.place(x=320,y=150)
+
+       self.button = tk.Button(self.root, text="4", height=2,width=4, font=('Arial',18))
+       self.button.place(x=20,y=250)
+
+       self.button = tk.Button(self.root, text="5", height=2,width=4, font=('Arial',18))
+       self.button.place(x=120,y=250)
+
+       self.button = tk.Button(self.root, text="6", height=2,width=4, font=('Arial',18))
+       self.button.place(x=220,y=250)
+
+       self.button = tk.Button(self.root, text="x", height=2,width=4, font=('Arial',18))
+       self.button.place(x=320,y=250)
+
+       self.button = tk.Button(self.root, text="1", height=2,width=4, font=('Arial',18))
+       self.button.place(x=20,y=350)
+
+       self.button = tk.Button(self.root, text="2", height=2,width=4, font=('Arial',18))
+       self.button.place(x=120,y=350)
+
+       self.button = tk.Button(self.root, text="3", height=2,width=4, font=('Arial',18))
+       self.button.place(x=220,y=350)
+
+       self.button = tk.Button(self.root, text="÷", height=2,width=4, font=('Arial',18))
+       self.button.place(x=320,y=350)
+
+       self.button = tk.Button(self.root, text="/", height=2,width=4, font=('Arial',18))
+       self.button.place(x=20,y=450)
+
+       self.button = tk.Button(self.root, text="0", height=2,width=4, font=('Arial',18))
+       self.button.place(x=120,y=450)
+
+       self.button = tk.Button(self.root, text=".", height=2,width=4, font=('Arial',18))
+       self.button.place(x=220,y=450)
+
+       self.button = tk.Button(self.root, text="=", height=2,width=4, font=('Arial',18))
+       self.button.place(x=320,y=450)
 
        self.root.mainloop()
 
